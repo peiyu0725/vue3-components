3da6986bc6732615bb01e38e748f41391bf9dce7
Pei-Yu <b20321115@gmail.com>
2023-07-07 15:59:26 +0800

    feat: add query stream url

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -6,12 +6,12 @@ const player = ref(null)
 const playerRef = ref(null)
 
 const initPlayer = () => {
-  // const url = 'rtsp://192.168.2.236:8557/h264'
+  const url = 'rtsp://192.168.2.236:8557/h264'
   if (flvjs.isSupported()) {
     player.value = flvjs.createPlayer({
       type: 'flv',
       isLive: true,
-      url: 'ws://localhost:8888/'
+      url: 'ws://localhost:8888/' + url
     }, { deferLoadAfterSourceOpen: false })
 
     player.value.on('error', (e) => {
@@ -22,18 +22,18 @@ const initPlayer = () => {
 
     try {
       player.value.load()
-      let controller = player.value._transmuxer._controller 
-      let wsLoader = controller._ioctl._loader 
-      var oldWsOnCompleteFunc = wsLoader._onComplete 
-      wsLoader._onComplete = function () { 
-        if (!controller._remuxer) { 
-          controller._remuxer = { 
-            flushStashedSamples: function () { 
+      let controller = player.value._transmuxer._controller
+      let wsLoader = controller._ioctl._loader
+      var oldWsOnCompleteFunc = wsLoader._onComplete
+      wsLoader._onComplete = function () {
+        if (!controller._remuxer) {
+          controller._remuxer = {
+            flushStashedSamples: function () {
               // _this.loadingVisiable = false 
-              console.log("flushStashedSamples") 
-            } 
-          } 
-        } oldWsOnCompleteFunc() 
+              console.log("flushStashedSamples")
+            }
+          }
+        } oldWsOnCompleteFunc()
       }
       player.value.play()
     } catch (error) {
